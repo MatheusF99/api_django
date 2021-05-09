02eaba8ccb27fb37ca1b644230f1f2d9f75b3f77
MatheusF99 <mc79846@gmail.com>
2021-05-09 19:00:06 -0300

    tabelas criadas, criando rotas

--- Todo_app/api/views.py
+++ Todo_app/api/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import decorators
+from rest_framework.views import APIView
 
 # Create your views here.
+
+
+class welcome(APIView):
+    def get(self, request, format=None):
+        content = {"hello": "world"}
+        return JsonResponse(content)
